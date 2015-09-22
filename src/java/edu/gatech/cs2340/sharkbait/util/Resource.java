7d6aa6036525d31166f06287b4bc44766735c2f8
Arihan Shah <arihan.shah@gmail.com>
2015-09-22 19:49:12 -0400

    stuff i changed in resources, property & property type

--- src/java/edu/gatech/cs2340/sharkbait/util/Resource.java
+++ src/java/edu/gatech/cs2340/sharkbait/util/Resource.java
@@ -4,4 +4,19 @@ package edu.gatech.cs2340.sharkbait.util;
  * Created by osama on 9/22/15.
  */
 public enum Resource {
+    Energy("Energy"),
+    Food("Food"),
+    Smithore("Smithore"),
+    Crystite("Crystite"),;
+
+    private String name;
+
+    Resource(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
