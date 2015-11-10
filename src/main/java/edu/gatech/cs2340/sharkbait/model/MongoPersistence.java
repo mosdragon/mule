6eae900fd245ccd72e01eb78bc77860181a7bca7
Osama Sakhi <msakhi21@gmail.com>
2015-11-09 23:45:42 -0500

    MongoPersistence checkstyle complete

--- src/main/java/edu/gatech/cs2340/sharkbait/model/MongoPersistence.java
+++ src/main/java/edu/gatech/cs2340/sharkbait/model/MongoPersistence.java
@@ -12,101 +12,143 @@ import edu.gatech.cs2340.trydent.log.Log;
 import org.bson.Document;
 
 /**
- * Created by osama on 10/25/15.
+ * Connets to MongoDB and handles all load/save operations.
  */
-public class MongoPersistence implements Persistence {
+public final class MongoPersistence {
 
-    private static MongoClient client;
-    private static MongoClientURI uri;
+  /**
+   * MasterController tag.
+   */
+  private static final String MASTER_CONTROLLER = "MasterController";
 
-    private static final int SINGLE_RESULT = 1;
+  /**
+   * GameConfigs tag.
+   */
+  private static final String GAME_CONFIGS = "GameConfigs";
 
-    private MongoPersistence() {}
+  /**
+   * GameDuration tag.
+   */
+  private static final String GAME_DURATION = "GameDuration";
 
-    private static void connect() {
-        uri = new MongoClientURI(Constants.MONGO_CONNECTION);
-        client = new MongoClient(uri);
-    }
+  /**
+   * Store tag.
+   */
+  private static final String STORE = "Store";
 
-    public static void saveGame() {
+  /**
+   * gameId tag.
+   */
+  private static final String GAME_ID = "gameId";
 
-        connect();
+  /**
+   * The actual mongo client that creates the connection to db.
+   */
+  private static MongoClient client;
 
-        GameSave save = new GameSave();
-        LocalGameSaves.addGameSave(save);
+  /**
+   * The connection uri for mongo.
+   */
+  private static MongoClientURI uri;
 
-        JsonObject gameState = new JsonObject();
+  /**
+   * Want only a single result from our find.
+   */
+  private static final int SINGLE_RESULT = 1;
 
-        gameState.addProperty(MASTER_CONTROLLER, MasterController.packAsJson());
-        gameState.addProperty(GAME_CONFIGS, GameConfigs.packAsJson());
-        gameState.addProperty(GAME_DURATION, GameDuration.packAsJson());
-        gameState.addProperty(STORE, Store.packAsJson());
-        gameState.addProperty(GAME_ID, MasterController.getGameId());
+  /**
+   * Private constructor to prevent instantiation.
+   */
+  private MongoPersistence() {
+  }
 
-        String gameStateJson = gameState.toString();
+  /**
+   * Helper method to connect to remote Mongo instance.
+   */
+  private static void connect() {
+      uri = new MongoClientURI(Constants.MONGO_CONNECTION);
+      client = new MongoClient(uri);
+  }
 
-        Log.debug("SAVE GAME - MongoPersistence");
-        Log.debug(gameStateJson);
+  /**
+   * Save the game entirely in its current state.
+   */
+  public static void saveGame() {
 
-//        TODO: Add code to save to DB using gameId
-        MongoDatabase database = client.getDatabase(Constants.DB_NAME);
+      connect();
 
-        Document document = Document.parse(gameStateJson);
+      GameSave save = new GameSave();
+      LocalGameSaves.addGameSave(save);
 
-        MongoCollection<Document> collection = database.getCollection(Constants.GAME_SAVES);
+      JsonObject gameState = new JsonObject();
 
-        if (collection != null) {
-            collection.insertOne(document);
-            Log.debug("Added it!");
-        }
+      gameState.addProperty(MASTER_CONTROLLER, MasterController.packAsJson());
+      gameState.addProperty(GAME_CONFIGS, GameConfigs.packAsJson());
+      gameState.addProperty(GAME_DURATION, GameDuration.packAsJson());
+      gameState.addProperty(STORE, Store.packAsJson());
+      gameState.addProperty(GAME_ID, MasterController.getGameId());
 
-    }
+      String gameStateJson = gameState.toString();
 
-    /**
-     * Pass in a gameId, and the fetch will be made from Mongo to get all the needed fields to
-     * reconstruct the game again
-     * @param gameSave, the actual gameSave used to retrieve the game
-     */
-    public static void loadGame(GameSave gameSave) {
-        connect();
-        MongoDatabase database = client.getDatabase(Constants.DB_NAME);
-        MongoCollection<Document> collection = database.getCollection(Constants.GAME_SAVES);
+      Log.debug("SAVE GAME - MongoPersistence");
+
+      MongoDatabase database = client.getDatabase(Constants.DB_NAME);
+      Document document = Document.parse(gameStateJson);
+      MongoCollection<Document> collection = database
+          .getCollection(Constants.GAME_SAVES);
+
+      if (collection != null) {
+          collection.insertOne(document);
+      }
+
+  }
+
+  /**
+   * Pass in a gameId, and the fetch will be made from Mongo to
+   * get all the needed fields to reconstruct the game again.
+   * @param gameSave the actual gameSave used to retrieve the game
+   */
+  public static void loadGame(final GameSave gameSave) {
+      connect();
+      MongoDatabase database = client.getDatabase(Constants.DB_NAME);
+      MongoCollection<Document> collection = database
+          .getCollection(Constants.GAME_SAVES);
 
 
-        FindIterable<Document> foundDocuments = collection
-                .find(new Document(GAME_ID, gameSave.getGameId()))
-                .limit(SINGLE_RESULT);
+      FindIterable<Document> foundDocuments = collection
+              .find(new Document(GAME_ID, gameSave.getGameId()))
+              .limit(SINGLE_RESULT);
 
-        Document gameSaveDocument = null;
+      Document gameSaveDocument = null;
 //        There will only be one document matching the gameId at most
-        for (Document doc : foundDocuments) {
-            gameSaveDocument = doc;
-        }
+      for (Document doc : foundDocuments) {
+          gameSaveDocument = doc;
+      }
 
-        if (gameSaveDocument != null) {
+      if (gameSaveDocument != null) {
 
-            String gameDuration = gameSaveDocument.getString(GAME_DURATION);
-            GameDuration.unpackfromJson(gameDuration);
+          String gameDuration = gameSaveDocument.getString(GAME_DURATION);
+          GameDuration.unpackfromJson(gameDuration);
 
-            String gameConfigs = gameSaveDocument.getString(GAME_CONFIGS);
-            GameConfigs.unpackfromJson(gameConfigs);
+          String gameConfigs = gameSaveDocument.getString(GAME_CONFIGS);
+          GameConfigs.unpackfromJson(gameConfigs);
 
-            String masterController = gameSaveDocument.getString(MASTER_CONTROLLER);
-            MasterController.unpackfromJson(masterController);
+          String masterController = gameSaveDocument
+              .getString(MASTER_CONTROLLER);
+          MasterController.unpackfromJson(masterController);
 
-            for (Player player : GameDuration.getPlayers()) {
-                player.unpack();
-            }
-            MasterController.initializeTimeline();
-            MasterController.resumeTime();
+          for (Player player : GameDuration.getPlayers()) {
+              player.unpack();
+          }
+          MasterController.initializeTimeline();
+          MasterController.resumeTime();
 
-            Log.debug("Everything deserialized");
+          Log.debug("Everything deserialized");
 
-        } else {
-            Log.debug("No result found. Starting New Game");
-        }
+      } else {
+          Log.debug("No result found. Starting New Game");
+      }
 
-    }
-
+  }
 
 }
