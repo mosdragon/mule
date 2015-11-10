29f9950b34ab9164ea0687c570ceb95cf5769fa4
Osama Sakhi <msakhi21@gmail.com>
2015-11-10 00:05:15 -0500

    MuleFactory and MuleDeserializer working

--- src/main/java/edu/gatech/cs2340/sharkbait/util/MuleDeserializer.java
+++ src/main/java/edu/gatech/cs2340/sharkbait/util/MuleDeserializer.java
@@ -1,37 +1,52 @@
 package edu.gatech.cs2340.sharkbait.util;
 
-import com.google.gson.*;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
 import edu.gatech.cs2340.sharkbait.model.Packer;
 import edu.gatech.cs2340.trydent.log.Log;
 
 import java.lang.reflect.Type;
 
 /**
- * Created by osama on 11/3/15.
+ * Custom deserialization of Mules from JSON.
  */
-public class MuleDeserializer implements JsonDeserializer<Mule> {
+public final class MuleDeserializer implements JsonDeserializer<Mule> {
 
+  /**
+   * type is a field every mule has.
+   */
     private static final String FIELD_MULETYPE = "type";
+
+  /**
+   * property is a field every mule has.
+   */
     private static final String FIELD_PROPERTY = "property";
 
-    @Override
-    public Mule deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext
-            jsonDeserializationContext) throws JsonParseException {
+  @Override
+  public Mule deserialize(final JsonElement jsonElement,
+                          final Type type, final JsonDeserializationContext
+                                  jsonDeserializationContext)
+      throws JsonParseException {
 
-        Mule created = null;
-        Log.debug(jsonElement.toString());
+    Mule created = null;
+    Log.debug(jsonElement.toString());
 
-        if (jsonElement.isJsonObject()) {
+    if (jsonElement.isJsonObject()) {
+      JsonObject json = jsonElement.getAsJsonObject();
+      if (json.has(FIELD_MULETYPE) && json.has(FIELD_PROPERTY)) {
 
-            JsonObject json = jsonElement.getAsJsonObject();
-            if (json.has(FIELD_MULETYPE) && json.has(FIELD_PROPERTY)) {
-                Resource muleType = Packer.unpack(json.get(FIELD_MULETYPE), Resource
-                        .class);
-                Property property = Packer.unpack(json.get(FIELD_PROPERTY), Property.class);
-                created =  MuleFactory.createMule(property, muleType);
-            }
-        }
+        Resource muleType = Packer.unpack(json.get(FIELD_MULETYPE),
+              Resource.class);
 
-        return created;
+        Property property = Packer.unpack(json.get(FIELD_PROPERTY),
+              Property.class);
+          created =  MuleFactory.createMule(property, muleType);
+      }
     }
+
+      return created;
+  }
 }
