b0381a53bd21b2f58444797860dd29a9c400cae8
Bruck Woldeyes <bwoldeyes3@gatech.edu>
2015-11-29 17:33:06 -0500

    added music to the game

--- src/main/java/edu/gatech/cs2340/sharkbait/MainApplication.java
+++ src/main/java/edu/gatech/cs2340/sharkbait/MainApplication.java
@@ -4,13 +4,32 @@ import edu.gatech.cs2340.sharkbait.controller.MasterController;
 import edu.gatech.cs2340.sharkbait.model.Constants;
 import edu.gatech.cs2340.trydent.log.Log;
 import javafx.application.Application;
+import javafx.scene.media.AudioClip;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import javax.swing.*;
+import java.io.File;
+import sun.audio.*;
+
 
 public class MainApplication extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         MasterController.initialize(primaryStage);
+        String path = "./src/music3.wav";
+        Media media = new Media(new File(path).toURI().toString());
+        MediaPlayer mediaPlayer = new MediaPlayer(media);
+        mediaPlayer.setAutoPlay(true);
+        mediaPlayer.onRepeatProperty();
+        mediaPlayer.setCycleCount(AudioClip.INDEFINITE);
+        MediaView mediaView = new MediaView(mediaPlayer);
+
+
+
     }
 
     public static void main(String[] args) {
